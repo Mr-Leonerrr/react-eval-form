5c4c99c2629404f955fd09c4f709e2d4e412a101
NATERA CHIQUILLO LEONARDO <lnatera@cuc.edu.co>
2022-04-25 19:12:37 -0500

    Feat: basic form base

---
+++ src/components/Form.jsx
@@ -0,0 +1,13 @@
+const Form = () => {
+    <>
+        <form>
+            <input type="text" placeholder="Name" />
+            <input type="text" placeholder="Email" />
+            <input type="text" placeholder="Phone" />
+            <textarea placeholder="Message"></textarea>
+            <button>Send</button>
+        </form>
+    </>
+};
+
+export default Form;
